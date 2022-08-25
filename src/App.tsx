b39154052dad4f9dd4a3f29fc84bb552d5552b4f
gracione <gracione2017@gmail.com>
2022-08-25 19:28:44 -0300

    alterando funcao para class

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,21 @@ import Card from './components/card/Card';
 import './global.css';
 import { Cartao, Container } from './styles';
 
-function App() {
-  let dadosCard = [{
-    "cliente" : "carlos",
-    "funcionario" : "pedro",
-    "tratamento" : "hidra",
-    "telefone" : "999998"
-  }];
 
-  return (
-    <Container>
-      <Card dados={dadosCard[0]} ></Card>
-    </Container>
-  );
+class App extends React.Component {
+  //  console.log(dadosCard[0].funcionario);
+  render() {
+    let dadosCard = [{
+      "cliente" : "carlos",
+      "funcionario" : "pedro",
+      "tratamento" : "hidra",
+      "telefone" : "999998"
+    }];
+    return (
+      <Container>
+        <Card {...dadosCard} />
+      </Container>
+    );
+  }
 }
 export default App;
--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,23 +1,25 @@
+import React from 'react';
 import { Cartao } from "./style";
 
 interface tipoDeDados {
-  dados: object;
+  dados: object[];
 }
 
-function Card({ dados }: tipoDeDados) {
-  console.log(dados);
-
-  return (
-    <>
-      <Cartao><div className='dados-horario'><h2>08:00</h2></div><div className='dados-usuario' >
-        <ul>
-           <li>cliente: </li>
-        </ul>
-      </div></Cartao>
-    </>
-//          <li>funcionario : {dados.funcionario}</li>
-//          <li>tratamento : {dados.tratamento}</li>
-//          <li>telefone : {dados.telefone}</li>
-  );
+class Card extends React.Component {
+  render() {
+    console.log(this.props);
+    return (
+      <>
+        <Cartao><div className='dados-horario'><h2>08:00</h2></div><div className='dados-usuario' >
+          <ul>
+             <li>cliente: </li>
+          </ul>
+        </div></Cartao>
+      </>
+  //          <li>funcionario : {dados.funcionario}</li>
+  //          <li>tratamento : {dados.tratamento}</li>
+  //          <li>telefone : {dados.telefone}</li>
+    );
+  }
 }
 export default Card;
