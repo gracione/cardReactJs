88274b0405e1013ae41a9cc2dd7bbdfcf703528f
gracione <gracione2017@gmail.com>
2022-08-25 07:27:14 -0300

    card

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Card from './Card';
 import './global.css';
 import { Cartao, Container } from './styles';
 
 function App() {
   return (
     <Container>
+      <Card variavel="12dsd" ></Card>
       <Cartao><div className='dados-horario'><h2>08:00</h2></div><div className='dados-usuario' >
         <ul>
           <li>cliente: gracione</li>
@@ -29,12 +31,7 @@ function App() {
           <li>tratamento: chapinha</li>
           </ul>
         </div></Cartao>
-      <Cartao><div className='dados-horario'><h2>08:00</h2></div><div className='dados-usuario' >
-        <ul>
-          <li>cliente: gracione</li>
-          <li>tratamento: chapinha</li>
-          </ul>
-        </div></Cartao>
+      <Cartao></Cartao>
     </Container>
   );
 }
---
+++ src/Card.tsx
@@ -0,0 +1,13 @@
+import './global.css';
+
+interface tipoDeDados {
+	variavel: string;
+}
+
+function Card({variavel}:tipoDeDados) {
+  return (
+    <>
+    oii</>
+  );
+}
+export default Card;
