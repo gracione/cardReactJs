36a74838fc6347474e91c8209b43c81f46013cd5
gracione <gracione2017@gmail.com>
2022-08-26 12:30:00 -0300

    adicionando nos card

--- src/App.tsx
+++ src/App.tsx
@@ -7,31 +7,33 @@ import { Cartao, Container } from './styles';
 function App() {
   let dadosCard = [
     {
-      "cliente" : "carlos",
-      "funcionario" : "pedro",
-      "tratamento" : "hidra",
-      "telefone" : "999998"
+      "cliente": "carlos",
+      "funcionario": "pedro",
+      "tratamento": "hidra",
+      "telefone": "999998"
     },
     {
-      "cliente" : "jose",
-      "funcionario" : "araujo",
-      "tratamento" : "hidra",
-      "telefone" : "555998"
+      "cliente": "jose",
+      "funcionario": "araujo",
+      "tratamento": "hidra",
+      "telefone": "555998"
     },
   ];
-  
-    for (let index = 0; index < dadosCard.length; index++) { 
-      console.log(dadosCard[index].cliente);
-     }
-    return (
-      <Container>
-        <Card
-            cliente={dadosCard[0].cliente}
-            funcionario={dadosCard[0].funcionario}
-            tratamento={dadosCard[0].tratamento}
-            telefone={dadosCard[0].telefone}
-            />
-      </Container>
-    );
+
+  let card = [];
+  for (let index = 0; index < dadosCard.length; index++) {
+    card[index] = <Card
+      cliente={dadosCard[index].cliente}
+      funcionario={dadosCard[index].funcionario}
+      tratamento={dadosCard[index].tratamento}
+      telefone={dadosCard[index].telefone}
+    />;
+  }
+
+  return (
+    <Container>
+      {card}
+    </Container>
+  );
 }
 export default App;
